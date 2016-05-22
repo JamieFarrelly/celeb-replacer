7b96e8d150a75a7d567913ded2b07639e06133c7
Jamie Farrelly <jamie.farrelly@paywithfire.com>
2016-05-22 15:59:26 +0100

    updating to replace celeb names

---
+++ dictionaries/original.js
@@ -0,0 +1,12 @@
+var dictionary={
+	"replacements": {
+		"Kim Jong Un" : "Kim Kardashian",
+		"Kim Jong-un" : "Kim Kardashian",
+		"Obama" : "Osama Bin Laden",
+		"Ed Sheeran" : "Rupert Grint",
+		"Wayne Rooney" : "Shrek"
+	},
+    
+    "expressions": {
+    }
+};
